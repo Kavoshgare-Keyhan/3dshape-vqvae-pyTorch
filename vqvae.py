d43e89967b6079faf96f04bb37149b171f92cd00
Mohsen <m.abghari@outlook.com>
2024-07-28 20:15:43 +0200

    turn off pre and post quantization and add comments

--- vqvae.py
+++ vqvae.py
@@ -11,21 +11,17 @@ class VQVAE(nn.Module):
                  ):
         super(VQVAE, self).__init__()
         self.encoder = get_encoder(config)
-        self.pre_quant_conv = nn.Conv2d(config['model_params']['convbn_channels'][-1],
-                                        config['model_params']['latent_dim'],
-                                        kernel_size=1)
+        # self.pre_quant_conv = nn.Conv2d(config['model_params']['convbn_channels'][-1], config['model_params']['latent_dim'], kernel_size=1)
         self.quantizer = get_quantizer(config)
-        self.post_quant_conv = nn.Conv2d(config['model_params']['latent_dim'],
-                                        config['model_params']['transposebn_channels'][0],
-                                        kernel_size=1)
+        # self.post_quant_conv = nn.Conv2d(config['model_params']['latent_dim'], config['model_params']['transposebn_channels'][0], kernel_size=1)
         self.decoder = get_decoder(config)
         
     def forward(self, x):
-        enc = self.encoder(x) # Call forward method by automatically passing x to the forward method withing Encoder class and returning its output which is called out and is a sequential of layers
-        quant_input = self.pre_quant_conv(enc)
-        quant_output, quant_loss, quant_idxs = self.quantizer(quant_input) # Call forward method and return corresponding outputs where quant_output is z_q
-        dec_input = self.post_quant_conv(quant_output)
-        out = self.decoder(dec_input)
+        enc = self.encoder(x) # Z_e - Call forward method by automatically passing an image as x variable to the forward method withing Encoder class and returning its output which is called out and is a sequential of layers
+        # quant_input = self.pre_quant_conv(enc) 
+        quant_output, quant_loss, quant_idxs = self.quantizer(enc) # Call forward method and return corresponding outputs where quant_output is z_q
+        # dec_input = self.post_quant_conv(quant_output)
+        out = self.decoder(quant_output)
         return {
             'generated_image' : out,
             'quantized_output' : quant_output,
@@ -34,7 +30,7 @@ class VQVAE(nn.Module):
         }
     
     def decode_from_codebook_indices(self, indices):
-        quantized_output = self.quantizer.quantize_indices(indices) # returns indices corresponds to q(z|x)
+        quantized_output = self.quantizer.quantize_indices(indices) # returns quantization output
         dec_input = self.post_quant_conv(quantized_output)
         return self.decoder(dec_input)
         
